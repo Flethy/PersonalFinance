5f020ce36251f6e5ef3e4996245f1882ae120728
Flethy <petrova.vera.ev@gmail.com>
2020-10-26 18:14:48 +0300

    add operations fragment and update data functions

--- app/src/main/java/ru/personalfinance/AccountsFragment.java
+++ app/src/main/java/ru/personalfinance/AccountsFragment.java
@@ -1,17 +1,38 @@
 package ru.personalfinance;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+import ru.personalfinance.Model.Data;
 
 public class AccountsFragment extends Fragment {
 
@@ -34,11 +55,31 @@ public class AccountsFragment extends Fragment {
 
     private Animation fadeOpen, fadeClose;
 
+    // income and expense result
+
+    private TextView totalIncomeResult;
+    private TextView totalExpenseResult;
+
+    // Firebase
+
+    private FirebaseAuth mAuth;
+//    private DatabaseReference mIncomeDatabase;
+//    private DatabaseReference mExpenseDatabase;
+    private DatabaseReference mChangeDatabase;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View myview = inflater.inflate(R.layout.fragment_accounts, container, false);
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser mUser = mAuth.getCurrentUser();
+        String uid = mUser.getUid();
+
+//        mIncomeDatabase = FirebaseDatabase.getInstance().getReference().child("IncomeData").child(uid);
+//        mExpenseDatabase = FirebaseDatabase.getInstance().getReference().child("ExpenseData").child(uid);
+        mChangeDatabase = FirebaseDatabase.getInstance().getReference().child("ChangeData").child(uid);
 
         // connect floating button to layout
 
@@ -51,6 +92,11 @@ public class AccountsFragment extends Fragment {
         fab_income_txt = myview.findViewById(R.id.income_ft_text);
         fab_expense_txt = myview.findViewById(R.id.expense_ft_text);
 
+        // Total income and expense result set
+
+        totalIncomeResult = myview.findViewById(R.id.income_set_result);
+        totalExpenseResult = myview.findViewById(R.id.expense_set_result);
+
         // animation connect
 
         fadeOpen = AnimationUtils.loadAnimation(getActivity(), R.anim.fade_open);
@@ -59,6 +105,8 @@ public class AccountsFragment extends Fragment {
         fab_main_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                addData();
 
                 if (isOpen) {
 
@@ -91,6 +139,240 @@ public class AccountsFragment extends Fragment {
             }
         });
 
+        // calculate total income
+
+        mChangeDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                int totalsum = 0;
+
+                for (DataSnapshot mysnap : snapshot.getChildren()) {
+                    Data data = mysnap.getValue(Data.class);
+                    if (data.getChange().equals("income")) {
+                        totalsum += data.getAmount();
+                    }
+                }
+
+                String strResult = String.valueOf(totalsum);
+                totalIncomeResult.setText(strResult);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        // calculate total expense
+
+        mChangeDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                int totalsum = 0;
+
+                for (DataSnapshot mysnap : snapshot.getChildren()) {
+                    Data data = mysnap.getValue(Data.class);
+                    if (data.getChange().equals("expense")) {
+                        totalsum += data.getAmount();
+                    }
+                }
+
+                String strResult = String.valueOf(totalsum);
+                totalExpenseResult.setText(strResult);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
         return myview;
     }
+
+    // Floating button animation
+
+    private void ftAnimation() {
+        if (isOpen) {
+
+            fab_income_btn.startAnimation(fadeClose);
+            fab_expense_btn.startAnimation(fadeClose);
+            fab_income_btn.setClickable(false);
+            fab_expense_btn.setClickable(false);
+
+            fab_income_txt.startAnimation(fadeClose);
+            fab_expense_txt.startAnimation(fadeClose);
+            fab_income_txt.setClickable(false);
+            fab_expense_txt.setClickable(false);
+            isOpen = false;
+
+        } else {
+
+            fab_income_btn.startAnimation(fadeOpen);
+            fab_expense_btn.startAnimation(fadeOpen);
+            fab_income_btn.setClickable(true);
+            fab_expense_btn.setClickable(true);
+
+            fab_income_txt.startAnimation(fadeOpen);
+            fab_expense_txt.startAnimation(fadeOpen);
+            fab_income_txt.setClickable(true);
+            fab_expense_txt.setClickable(true);
+            isOpen = true;
+
+        }
+    }
+
+    private void addData() {
+        // Fab button income
+
+        fab_income_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                incomeDataInsert();
+            }
+        });
+
+        fab_expense_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                expenseDataInsert();
+            }
+        });
+    }
+
+    public void incomeDataInsert() {
+
+        AlertDialog.Builder mydialog = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = LayoutInflater.from(getActivity());
+        View myview = inflater.inflate(R.layout.custom_layout_for_insertdata, null);
+        mydialog.setView(myview);
+        final AlertDialog dialog = mydialog.create();
+
+        dialog.setCancelable(false);
+
+        final EditText edtAmount = myview.findViewById(R.id.amount_edt);
+        final EditText edtNote = myview.findViewById(R.id.note_edt);
+
+        String[] incomeType = {"Зарплата", "Сбережения", "Подарки", "Премия", "Проценты", "Другое"};
+
+        // spinner
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, incomeType);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        final Spinner edtType = myview.findViewById(R.id.type_edt);
+        edtType.setAdapter(adapter);
+        edtType.setPrompt("Title");
+        edtType.setSelection(0);
+
+        Button btnSave = myview.findViewById(R.id.btnSave);
+        Button btnCancel = myview.findViewById(R.id.btnCancel);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String type = edtType.getSelectedItem().toString().trim();
+                String amount = edtAmount.getText().toString().trim();
+                String note = edtNote.getText().toString().trim();
+
+                if (TextUtils.isEmpty(amount)) {
+                    edtAmount.setError("Обязательное поле");
+                    return;
+                }
+
+                int ourAmountInt = Integer.parseInt(amount);
+
+                String id = mChangeDatabase.push().getKey();
+                String mDate = DateFormat.getDateInstance().format(new Date());
+                Data data = new Data(ourAmountInt, type, "income", note, id, mDate);
+
+                mChangeDatabase.child(id).setValue(data);
+                Toast.makeText(getActivity(), "Данные сохранены", Toast.LENGTH_SHORT).show();
+
+                ftAnimation();
+                dialog.dismiss();
+
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ftAnimation();
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+
+    }
+
+    public void expenseDataInsert() {
+
+        AlertDialog.Builder mydialog = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = LayoutInflater.from(getActivity());
+        View myview = inflater.inflate(R.layout.custom_layout_for_insertdata, null);
+        mydialog.setView(myview);
+
+        final AlertDialog dialog = mydialog.create();
+
+        dialog.setCancelable(false);
+
+        final EditText amount = myview.findViewById(R.id.amount_edt);
+        final EditText note = myview.findViewById(R.id.note_edt);
+
+        String[] expenseType = {"Продукты", "Семья", "Транспорт", "Досуг", "Кафе", "Здоровье", "Покупки", "Поездки", "Спорт", "Другое"};
+
+        // spinner
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, expenseType);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        final Spinner edtType = myview.findViewById(R.id.type_edt);
+        edtType.setAdapter(adapter);
+        edtType.setPrompt("Title");
+        edtType.setSelection(0);
+
+        Button btnSave = myview.findViewById(R.id.btnSave);
+        Button btnCancel = myview.findViewById(R.id.btnCancel);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String tmAmount = amount.getText().toString().trim();
+                String tmType = edtType.getSelectedItem().toString().trim();
+                String tmNote = note.getText().toString().trim();
+
+                if (TextUtils.isEmpty(tmAmount)) {
+                    amount.setError("Обязательное поле");
+                    return;
+                }
+
+                int inAmount = Integer.parseInt(tmAmount);
+
+                String id = mChangeDatabase.push().getKey();
+                String mDate = DateFormat.getDateInstance().format(new Date());
+
+                Data data = new Data(inAmount, tmType, "expense", tmNote, id, mDate);
+                mChangeDatabase.child(id).setValue(data);
+                Toast.makeText(getActivity(), "Данные сохранены", Toast.LENGTH_SHORT).show();
+
+                ftAnimation();
+                dialog.dismiss();
+
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ftAnimation();
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+
+    }
+
 }
--- app/src/main/java/ru/personalfinance/OperationsFragment.java
+++ app/src/main/java/ru/personalfinance/OperationsFragment.java
@@ -1,64 +1,318 @@
 package ru.personalfinance;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link OperationsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+import ru.personalfinance.Model.Data;
+
 public class OperationsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    // Firebase database
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mIncomeDatabase;
+    private DatabaseReference mExpenseDatabase;
+    private DatabaseReference mChangeDatabase;
 
-    public OperationsFragment() {
-        // Required empty public constructor
-    }
+    // Recyclerview
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment OperationsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static OperationsFragment newInstance(String param1, String param2) {
-        OperationsFragment fragment = new OperationsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private RecyclerView recyclerView;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    // TextView
+
+    private TextView incomeTotalSum;
+    private TextView expenseTotalSum;
+
+    // Update EditText
+
+    private EditText edtAmount;
+    private Spinner edtType;
+    private EditText edtNote;
+
+    // Button for update and delete
+
+    private Button btnUpdate;
+    private Button btnDelete;
+
+    // Data item value
+
+    private String type;
+    private String note;
+    private String change;
+    private int amount;
+
+    private String post_key;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_operations, container, false);
+        View myview = inflater.inflate(R.layout.fragment_operations, container, false);
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser mUser = mAuth.getCurrentUser();
+        String uid = mUser.getUid();
+
+//        mIncomeDatabase = FirebaseDatabase.getInstance().getReference().child("IncomeData").child(uid);
+//        mExpenseDatabase = FirebaseDatabase.getInstance().getReference().child("ExpenseData").child(uid);
+        mChangeDatabase = FirebaseDatabase.getInstance().getReference().child("ChangeData").child(uid);
+
+        incomeTotalSum = myview.findViewById(R.id.income_txt_result);
+        expenseTotalSum = myview.findViewById(R.id.expense_txt_result);
+
+        recyclerView = myview.findViewById(R.id.recycler_id_income);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+
+        layoutManager.setReverseLayout(true);
+        layoutManager.setStackFromEnd(true);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+
+        mChangeDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                int totalValue = 0;
+
+                for (DataSnapshot mySnapshot : snapshot.getChildren()) {
+
+                    Data data = mySnapshot.getValue(Data.class);
+                    if (data.getChange().equals("income")) {
+                        totalValue += data.getAmount();
+                    }
+                    String strTotalValue = String.valueOf(totalValue);
+                    incomeTotalSum.setText(strTotalValue);
+
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+
+        mChangeDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                int totalValue = 0;
+
+                for (DataSnapshot mySnapshot : snapshot.getChildren()) {
+
+                    Data data = mySnapshot.getValue(Data.class);
+                    if (data.getChange().equals("expense")) {
+                        totalValue += data.getAmount();
+                    }
+                    String strTotalValue = String.valueOf(totalValue);
+                    expenseTotalSum.setText(strTotalValue);
+
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+
+
+        return myview;
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerAdapter<Data, MyViewHolder> adapter = new FirebaseRecyclerAdapter<Data, MyViewHolder>
+                (
+                        Data.class,
+                        R.layout.income_recycler_data,
+                        MyViewHolder.class,
+                        mChangeDatabase
+                ) {
+            @Override
+            protected void populateViewHolder(MyViewHolder myViewHolder, final Data model, final int position) {
+
+                myViewHolder.setType(model.getType());
+                myViewHolder.setNote(model.getNote());
+                myViewHolder.setDate(model.getDate());
+                myViewHolder.setAmount(model.getAmount(), model.getChange());
+
+                myViewHolder.mView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        post_key = getRef(position).getKey();
+
+                        type = model.getType();
+                        note = model.getNote();
+                        amount = model.getAmount();
+                        change = model.getChange();
+
+                        updateDataItem();
+                    }
+                });
+
+            }
+        };
+
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        View mView;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
+
+        private void setType(String type) {
+            TextView mType = mView.findViewById(R.id.type_txt_income);
+            mType.setText(type);
+        }
+
+        private void setNote(String note) {
+            TextView mNote = mView.findViewById(R.id.note_txt_income);
+            mNote.setText(note);
+        }
+
+        private void setDate(String date) {
+            TextView mDate = mView.findViewById(R.id.date_txt_income);
+            mDate.setText(date);
+        }
+
+        @SuppressLint("ResourceAsColor")
+        private void setAmount(int amount, String change) {
+            TextView mAmount = mView.findViewById(R.id.amount_txt_income);
+            String strAmount = String.valueOf(amount);
+            if (change.equals("income")) {
+                mAmount.setText(String.format("+ %s ₽", strAmount));
+                mAmount.setTextColor(Color.parseColor("#699A68"));
+            } else if (change.equals("expense")) {
+                mAmount.setText(String.format("%s ₽", strAmount));
+            }
+        }
+
+    }
+
+    private void updateDataItem() {
+
+        AlertDialog.Builder mydialog = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = LayoutInflater.from(getActivity());
+        View myview = inflater.inflate(R.layout.update_data_item, null);
+        mydialog.setView(myview);
+
+        edtAmount = myview.findViewById(R.id.amount_edt);
+        edtType = myview.findViewById(R.id.type_edt);
+        edtNote = myview.findViewById(R.id.note_edt);
+
+        String[] incomeType = {"Зарплата", "Сбережения", "Подарки", "Премия", "Проценты", "Другое"};
+        String[] expenseType = {"Продукты", "Семья", "Транспорт", "Досуг", "Кафе", "Здоровье", "Покупки", "Поездки", "Спорт", "Другое"};
+
+        // spinner
+        ArrayAdapter<String> adapter;
+        if (change.equals("income")) {
+            adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, incomeType);
+        } else {
+            adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, expenseType);
+        }
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        final Spinner edtType = myview.findViewById(R.id.type_edt);
+        edtType.setAdapter(adapter);
+        edtType.setPrompt("Title");
+        int spinnerPosition = adapter.getPosition(type);
+        edtType.setSelection(spinnerPosition);
+
+        // set data to EditText
+
+//        edtType.setSelected(type);
+//        edtType.setSelection(type.length());
+
+        edtNote.setText(note);
+        edtNote.setSelection(note.length());
+
+        edtAmount.setText(String.valueOf(amount));
+        edtAmount.setSelection(String.valueOf(amount).length());
+
+        btnUpdate = myview.findViewById(R.id.btnUpdate);
+        btnDelete = myview.findViewById(R.id.btnDelete);
+
+        final AlertDialog dialog = mydialog.create();
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                type = edtType.getSelectedItem().toString().trim();
+                note = edtNote.getText().toString().trim();
+                String mAmount = String.valueOf(amount);
+                mAmount = edtAmount.getText().toString().trim();
+                int myAmount = Integer.parseInt(mAmount);
+                String mDate = DateFormat.getDateInstance().format(new Date());
+                Data data = new Data(myAmount, type, change, note, post_key, mDate);
+
+                mChangeDatabase.child(post_key).setValue(data);
+//                if (change.equals("income")) {
+//                    mIncomeDatabase.child(post_key).setValue(data);
+//                } else if (change.equals("expense")) {
+//                    mExpenseDatabase.child(post_key).setValue(data);
+//                }
+
+                dialog.dismiss();
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                if (change.equals("income")) {
+//                    mIncomeDatabase.child(post_key).removeValue();
+//                } else if (change.equals("expense")) {
+//                    mExpenseDatabase.child(post_key).removeValue();
+//                }
+//                mIncomeDatabase.child(post_key).removeValue();
+//                mExpenseDatabase.child(post_key).removeValue();
+                mChangeDatabase.child(post_key).removeValue();
+
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+
+    }
+
 }
